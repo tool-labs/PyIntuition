1279672a53b55620698c61150e504c280a3083b1
Robin Krahl <me@robin-krahl.de>
2013-12-03 16:06:44 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='PyIntuition',
+    version='0.1dev',
+    author='Robin Krahl',
+    author_email='me@robin-krahl.de',
+    packages=['pyintuition'],
+    license='LICENSE',
+    long_description=open('README.md'),
+)
